0a8f2ad8f8e4eb9716053aee0deb516b39b93097
Hanny Biggsmen <hannah.biggam@gmail.com>
2021-07-11 18:05:49 +0200

    pair programming:  adding needed icons, adding new nav components, added the homepage

--- client/src/App.js
+++ client/src/App.js
@@ -8,23 +8,19 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import SingleThought from './pages/SingleThought';
-import Profile from './pages/Profile';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 
 // import Home from './pages/Home';
 // import IssuesPage from './pages/IssuesPage';
-// import Login from './pages/Login';
 // import IssuesSubmissionPage from './pages/IssuesSubmissionPage';
 // import MainPage from './pages/MainPage';
 // import RosterStatus from './pages/RosterStatus';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
 // import DriverForm from './components/DriverForm';
 // import DriverList from './components/DriverList';
 
@@ -69,15 +65,6 @@ function App() {
             </Route>
             <Route exact path="/signup">
               <Signup />
-            </Route>
-            <Route exact path="/me">
-              <Profile />
-            </Route>
-            <Route exact path="/profiles/:username">
-              <Profile />
-            </Route>
-            <Route exact path="/thoughts/:thoughtId">
-              <SingleThought />
             </Route>
           </div>
           <Footer />
---
+++ client/src/components/NavPostLogin/index.js
@@ -0,0 +1,124 @@
+class DesktopContainer extends Component {
+    state = {}
+  
+    hideFixedMenu = () => this.setState({ fixed: false })
+    showFixedMenu = () => this.setState({ fixed: true })
+  
+    render() {
+      const { children } = this.props
+      const { fixed } = this.state
+  
+      return (
+        <Media greaterThan='mobile'>
+          <Visibility
+            once={false}
+            onBottomPassed={this.showFixedMenu}
+            onBottomPassedReverse={this.hideFixedMenu}
+          >
+            <Segment
+              inverted
+              textAlign='center'
+              style={{ minHeight: 700, padding: '1em 0em' }}
+              vertical
+            >
+              <Menu
+                fixed={fixed ? 'top' : null}
+                inverted={!fixed}
+                pointing={!fixed}
+                secondary={!fixed}
+                size='large'
+              >
+                <Container>
+                  <Menu.Item as='a' active>
+                    Home
+                  </Menu.Item>
+                  <Menu.Item as='a'>Work</Menu.Item>
+                  <Menu.Item as='a'>Company</Menu.Item>
+                  <Menu.Item as='a'>Careers</Menu.Item>
+                  <Menu.Item position='right'>
+                    <Button as='a' inverted={!fixed}>
+                      Logout
+                    </Button>
+                  </Menu.Item>
+                </Container>
+              </Menu>
+              <HomepageHeading />
+            </Segment>
+          </Visibility>
+  
+          {children}
+        </Media>
+      )
+    }
+  }
+  
+  DesktopContainer.propTypes = {
+    children: PropTypes.node,
+  }
+  
+  class MobileContainer extends Component {
+    state = {}
+  
+    handleSidebarHide = () => this.setState({ sidebarOpened: false })
+  
+    handleToggle = () => this.setState({ sidebarOpened: true })
+  
+    render() {
+      const { children } = this.props
+      const { sidebarOpened } = this.state
+  
+      return (
+        <Media as={Sidebar.Pushable} at='mobile'>
+          <Sidebar.Pushable>
+            <Sidebar
+              as={Menu}
+              animation='overlay'
+              inverted
+              onHide={this.handleSidebarHide}
+              vertical
+              visible={sidebarOpened}
+            >
+              <Menu.Item as='a' active>
+                Home
+              </Menu.Item>
+              <Menu.Item as='a'>Work</Menu.Item>
+              <Menu.Item as='a'>Company</Menu.Item>
+              <Menu.Item as='a'>Careers</Menu.Item>
+              <Menu.Item as='a'>Logout</Menu.Item>
+            </Sidebar>
+  
+            <Sidebar.Pusher dimmed={sidebarOpened}>
+              <Segment
+                inverted
+                textAlign='center'
+                style={{ minHeight: 350, padding: '1em 0em' }}
+                vertical
+              >
+                <Container>
+                  <Menu inverted pointing secondary size='large'>
+                    <Menu.Item onClick={this.handleToggle}>
+                      <Icon name='sidebar' />
+                    </Menu.Item>
+                    <Menu.Item position='right'>
+                      <Button as='a' inverted>
+                        Logout
+                      </Button>
+                    </Menu.Item>
+                  </Menu>
+                </Container>
+                <HomepageHeading mobile />
+              </Segment>
+  
+              {children}
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
+        </Media>
+      )
+    }
+  }
+  
+  MobileContainer.propTypes = {
+    children: PropTypes.node,
+  }
+
+export default NavPostLogin;
