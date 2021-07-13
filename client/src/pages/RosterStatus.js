53dbf90c91334f749d022ee6b55d976487a49dc4
Richard Yvarra <richard.yvarra@gmail.com>
2021-07-13 19:10:21 -0400

    some changes

--- client/src/pages/RosterStatus.js
+++ client/src/pages/RosterStatus.js
@@ -5,27 +5,33 @@ import { CURRENT_ROSTER} from '../utils/queries'
 // import Auth from '../utils/auth';
 
 
-const RosterStatus = () => {
-    const { loading, data} = useQuery(CURRENT_ROSTER)
+const RosterStatus = (prop) => {
+    const { loading, data} = useQuery(CURRENT_ROSTER, {
+      variables:{busNumber: 330}
+    })
     const roster = data?.roster || [];
 
 
 
-return (
 
-   
-<List>
-<Header as='h1' block> Roster Status </Header>
-  
-  <List.Item>
-    
-    
-  </List.Item>
-  
-</List>
-  
+console.log(data)
 
+    const renderStudents = () => {
+      console.log(data)
+   return data.currentRoster.map((s) => {
+        return <List.Item>{s.name}</List.Item>
 
-) 
+    })
+  }
+
+if(data === undefined){
+  return <h1>Loading</h1>
+}
+  return <div>
+    <Header>ROSTER STATUS:</Header>
+    <h2>{prop.busNumber}</h2>
+      <List>{renderStudents()}</List>
+    </div>
+
 }
 export default RosterStatus
