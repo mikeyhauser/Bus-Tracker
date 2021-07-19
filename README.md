64c3c0a3340c45afd88bd9046b1ae31d3b0b8d46
Hanny Biggsmen <hannah.biggam@gmail.com>
2021-07-20 00:18:53 +0200

    adjusting home page, adding start of ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Bus-Tracker
-Mikey, Stevenson, Hannah, and Richard
+# Bus Tracker
+
+[Application Deployed via Heroku](https://magic-school-bus-tracker.herokuapp.com/)
+
+A web application that tracks the status of buses. Keep track of the children on the bus and gives bus drivers/admin a way to keep track of the children as well as any mechanical issues the bus has. As a bus driver or school admin I want an easy and centralized way to keep track of the children on the bus and also automatically inform parents of their whereabouts or any disruptions in the route.
+
+This application is a simple note taker. It allows the user to take and store notes, as well as delete existing notes.
+The application saves and retrieves note data from a JSON file.
+
+<img src= "....">
+
+## Application Features:
+- Bus number input that adjust route stops and students
+- Roster status page which shows a list of present and absent students
+- Mechanical issues submission page
+- Mechanical issues status page for an overview of all issues under one organization
+[Include images here for each of the features or a gif?]
+
+## Technology used:
+- GraphQL
+- Node.js
+- Express.js
+- MongoDB
+- Mongoose ODM
+- Semantic CSS React
+- Javascript
+- JSX
+
+## Functionality:
+- JWT authentication
+- Mobile-friend UI
+- Sensitive API key information protection
+
+## Process Notes
+The files had to be adjusted slightly in the end so that it would deploy correctly on Heroku. The live URL can be accessed via the link at the top of the page.
+
+## Future Development
+- Admin specific view: In order to add new children to routes and adjust from an admin perspective, it would be beneficial to develop an admin side of the application....
+- Dark/Light mode
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,223 +8,15 @@ import {
   Divider,
   Grid,
   Header,
-  Icon,
   Image,
-  List,
-  Menu,
   Segment,
   Reveal,
-  Sidebar,
-  Visibility,
 } from 'semantic-ui-react';
 
 
 
-
-
-
-const { MediaContextProvider, Media } = createMedia({
-  breakpoints: {
-    mobile: 0,
-    tablet: 768,
-    computer: 1024,
-  },
-})
-
-// HEADER COMPONENT
-
-const HomepageHeading = ({ mobile }) => (
-  <Container text>
-    <Header
-      as='h1'
-      content='Imagine-a-Company'
-      inverted
-      style={{
-        fontSize: mobile ? '2em' : '4em',
-        fontWeight: 'normal',
-        marginBottom: 0,
-        marginTop: mobile ? '1.5em' : '3em',
-      }}
-    />
-    <Header
-      as='h2'
-      content='Do whatever you want when you want to.'
-      inverted
-      style={{
-        fontSize: mobile ? '1.5em' : '1.7em',
-        fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
-      }}
-    />
-    <Button as ={ Link } to='/RosterStatus'primary size='huge'>
-
-       Roster Status
-      <Icon name='right arrow' />
-    </Button>
-  </Container>
-)
-
-HomepageHeading.propTypes = {
-  mobile: PropTypes.bool,
-}
-
-/* Heads up!
- * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
- * It can be more complicated, but you can create really flexible markup.
- */
-
-
-
-
-// NAV BAR COMPONENT
-
-class DesktopContainer extends Component {
-  state = {}
-
-  hideFixedMenu = () => this.setState({ fixed: false })
-  showFixedMenu = () => this.setState({ fixed: true })
-
-  render() {
-    const { children } = this.props
-    const { fixed } = this.state
-
-    return (
-      <Media greaterThan='mobile'>
-        <Visibility
-          once={false}
-          onBottomPassed={this.showFixedMenu}
-          onBottomPassedReverse={this.hideFixedMenu}
-        >
-          <Segment
-            inverted
-            textAlign='center'
-            style={{ minHeight: 700, padding: '1em 0em' }}
-            vertical
-          >
-            <Menu
-              fixed={fixed ? 'top' : null}
-              inverted={!fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size='large'
-            >
-              {/* <Container>
-                <Menu.Item as='a' active>
-                  Home
-                </Menu.Item>
-                <Menu.Item as='a'>Work</Menu.Item>
-                <Menu.Item as='a'>Company</Menu.Item>
-                <Menu.Item as='a'>Careers</Menu.Item>
-                <Menu.Item position='right'>
-                  <Button as='a' inverted={!fixed}>
-                    Log in
-                  </Button>
-                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
-                    Sign Up
-                  </Button>
-                </Menu.Item>
-              </Container> */}
-            </Menu>
-            {/* <HomepageHeading /> */}
-          </Segment>
-        </Visibility>
-
-        {children}
-      </Media>
-    )
-  }
-}
-
-DesktopContainer.propTypes = {
-  children: PropTypes.node,
-}
-
-class MobileContainer extends Component {
-  state = {}
-
-  handleSidebarHide = () => this.setState({ sidebarOpened: false })
-
-  handleToggle = () => this.setState({ sidebarOpened: true })
-
-  render() {
-    const { children } = this.props
-    const { sidebarOpened } = this.state
-
-    return (
-      <Media as={Sidebar.Pushable} at='mobile'>
-        <Sidebar.Pushable>
-          <Sidebar
-            as={Menu}
-            animation='overlay'
-            inverted
-            onHide={this.handleSidebarHide}
-            vertical
-            visible={sidebarOpened}
-          >
-            <Menu.Item as='a' active>
-              Home
-            </Menu.Item>
-            <Menu.Item as='a'>Work</Menu.Item>
-            <Menu.Item as='a'>Company</Menu.Item>
-            <Menu.Item as='a'>Roster Status</Menu.Item>
-            {/* <Menu.Item as='a'>Log in</Menu.Item>
-            <Menu.Item as='a'>Sign Up</Menu.Item> */}
-          </Sidebar>
-
-          <Sidebar.Pusher dimmed={sidebarOpened}>
-            <Segment
-              inverted
-              textAlign='center'
-              style={{ minHeight: 350, padding: '1em 0em' }}
-              vertical
-            >
-              <Container>
-                <Menu inverted pointing secondary size='large'>
-                  <Menu.Item onClick={this.handleToggle}>
-                    <Icon name='sidebar' />
-                  </Menu.Item>
-                  <Menu.Item position='right'>
-                    <Button as='a' inverted>
-                      Log in
-                    </Button>
-                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-                      Sign Up
-                    </Button>
-                  </Menu.Item>
-                </Menu>
-              </Container>
-              {/* <HomepageHeading mobile /> */}
-            </Segment>
-
-            {children}
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </Media>
-    )
-  }
-}
-
-MobileContainer.propTypes = {
-  children: PropTypes.node,
-}
-
-const ResponsiveContainer = ({ children }) => (
-  /* Heads up!
-   * For large applications it may not be best option to put all page into these containers at
-   * they will be rendered twice for SSR.
-   */
-  <MediaContextProvider>
-    <DesktopContainer>{children}</DesktopContainer>
-    <MobileContainer>{children}</MobileContainer>
-  </MediaContextProvider>
-)
-
-ResponsiveContainer.propTypes = {
-  children: PropTypes.node,
-}
-
 const HomepageLayout = () => (
-  <ResponsiveContainer >
+  <Container >
     <Segment style={{ padding: '8em 0em' }} vertical>
       <Grid container stackable verticalAlign='middle'>
         <Grid.Row>
@@ -259,7 +51,6 @@ const HomepageLayout = () => (
             </p>
           </Grid.Column>
           <Grid.Column floated='right' width={6}>
-
           <Reveal animated='rotate'>
     <Reveal.Content visible>
       <Image circular size='large' src='busiconnontransparent.png' />
@@ -268,7 +59,7 @@ const HomepageLayout = () => (
       <Image circular size='medium' src='boy.png' />
     </Reveal.Content>
   </Reveal>
-          </Grid.Column>
+          </Grid.Column >
         </Grid.Row>
         <Grid.Row>
           <Grid.Column textAlign='center'>
@@ -298,7 +89,7 @@ const HomepageLayout = () => (
     </Segment>
 
 
-  </ResponsiveContainer>
+  </Container>
 )
 
 export default HomepageLayout
